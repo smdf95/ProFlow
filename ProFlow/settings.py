9adae1f3247dd14e31e9bcfaa92f788d9a3d8532
smdf95 <s.fisher95@hotmail.com>
2024-02-10 21:29:32 +0000

    add req.txt

--- ProFlow/settings.py
+++ ProFlow/settings.py
@@ -25,9 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS').split(' ')]
+# Retrieve ALLOWED_HOSTS from environmental variables
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+
+
+# Split the string by spaces and convert to list if the variable is set
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ALLOWED_HOSTS.split(' ')
+else:
+    # Set a default value if ALLOWED_HOSTS is not set
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -91,7 +100,6 @@ DATABASES = {
 
 database_url = os.environ.get("DATABASE_URL")
 
-DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
